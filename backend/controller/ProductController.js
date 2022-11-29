154e2cbe962d9ec5fd2c6cf847928f8d9ce47693
Stephen Moore <judo2000@comcast.net>
2022-11-29 13:31:38 -0700

    Create update product functionality

--- backend/controller/ProductController.js
+++ backend/controller/ProductController.js
@@ -10,10 +10,32 @@ exports.createProduct = async (req, res, next) => {
   });
 };
 
+// GET all products
 exports.getAllProducts = async (req, res) => {
   const products = await Product.find();
   res.status(200).json({
     success: true,
     products,
+  });
+};
+
+// Update Product --- Admin
+exports.updateProduct = async (req, res) => {
+  let product = await Product.findById(req.params.id);
+  if (!product) {
+    return res.status(500).json({
+      success: false,
+      message: 'Product was not found.',
+    });
+  }
+
+  product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+    useUnified: false,
+  });
+  res.status(200).json({
+    success: true,
+    product,
   });
 };
