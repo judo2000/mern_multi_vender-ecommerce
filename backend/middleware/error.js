d91bdc9b4a9e3989a747b617edbb01c3cd8c3983
Stephen Moore <judo2000@comcast.net>
2022-11-29 15:28:43 -0700

    Create error handleing for uncaught exception and unhandled promise rejection in server.js

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -3,6 +3,11 @@ const errorHandler = require('../utils/ErrorHandler');
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.message = err.message || 'Internal server error';
+  // Wrong mongodb id error
+  if (err.name === 'CastError') {
+    const message = `Resource not found with this id..Invalid ${err.path}`;
+    err = new errorHandler(message, 400);
+  }
 
   res.status(err.statusCode).json({
     success: false,
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,12 @@ const app = require('./app');
 const dotenv = require('dotenv');
 const connectDB = require('./db/Database');
 
+// Handling uncaught exception
+process.on('uncaughtException', (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log(`Shutting down the server uncaught exception!`);
+});
+
 // config
 dotenv.config({
   path: 'backend/config/.env',
@@ -13,4 +19,15 @@ connectDB();
 // create server
 const server = app.listen(process.env.PORT, () => {
   console.log(`Server listening on http://localhost${process.env.PORT}`);
+});
+
+// unhandled promise rejection
+
+process.on('unhandledRejection', (err) => {
+  console.log(`Shutting down the server for ${err.message}`);
+
+  console.log(`Shutting down the server due to unhandled promise rejection.`);
+  server.close(() => {
+    process.exit(1);
+  });
 });
